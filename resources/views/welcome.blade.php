bbbfaca3c75862f028e91d90aabf83fa64f6d72f
Etienne <etienne.perreau@edu.ece.fr>
2020-04-20 23:03:31 +0200

    Ajout de la page d'accueil qui affiche la photo + nom

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,39 @@
-
+@foreach($user as $GetPost)
 @extends('layouts.app')
 
 
 @section('title')
-                ECE ebay
+Bienvenue sur ECE EBAY  
+
+    {{$GetPost->name}}          
+
 @stop
 
 @section('content')
 
-@stop
 
+
+
+
+
+					<center>
+                    <img src="{{asset($GetPost->avatar)}}" style="height:600px;width:600px; margin-right:15px">
+                
+                    
+                       
+                    
+                    </center>
+
+                     
+
+
+             
+
+
+@stop
+       
+                  
 @section('footer')
 copy rights by ECE ebay
-@stop
+@stop                  
+  @endforeach
--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
+
+
+Route::get('/','PostController@GetAll');
 
 Route::get('/profile', 'InfosController@GetInfo');
 Route::post('renseigner','InfosController@save');
